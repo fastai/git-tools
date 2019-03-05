3db1afabd9a869aa9ae646616dfda1975b74ec23
Gokkul Nath TS <Gokkulnath@users.noreply.github.com>
2019-03-05 11:49:58 +0530

    Update hub-install.py

--- hub-install.py
+++ hub-install.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+This program will install hub tool based on the users platform.
+usage:
+python hub-install.py
+"""
 
 import os,requests,platform,json,subprocess
 import tarfile
